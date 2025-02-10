1eb8cb03de1f99e845b9808300bb15680cc1fbb2
Pulkit0111 <ptyagi1551@gmail.com>
2025-02-10 21:42:58 +0530

    added backend service and initial frontend bundler

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const {connectToDB} = require('./config/dbConfig')
+const {userRouter} = require("./routes/user.routes")
+const {todoRouter} = require("./routes/todo.routes")
+require("dotenv").config()
+
+const PORT = process.env.port || 5000
+const app = express()
+
+app.use(express.json())
+app.use("/users", userRouter)
+app.use("/todos", todoRouter)
+
+
+app.listen(PORT, ()=>{
+    connectToDB()
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
